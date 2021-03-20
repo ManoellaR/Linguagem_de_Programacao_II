f9b307171c669e7a184eea52526caaa9fefdc4c5
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-03-19 21:06:38 -0300

    Correção do Bug causado pelo NDEBUG e a dupla definição do construtor do TRIANGLE

--- T5/Shapes.hpp
+++ T5/Shapes.hpp
@@ -1,5 +1,6 @@
 #ifndef SHAPES_HPP
 #define SHAPES_HPP
+#define NDEBUG 1 // dev = 1 release = 0
 
 #include <iostream>
 #include <sstream>
@@ -7,102 +8,101 @@
 #include <iomanip>
 #include <math.h>
 
-#include "GenericElement.hpp"
-
 using namespace std;
 
-#define NDEBUG 1 // dev = 1 release = 0
-
-class Point2d{
+class Point2d
+{
 public:
     float m_x, m_y;
-    Point2d() : m_x(0), m_y(0) {};
-    Point2d(float x, float y) : m_x(x), m_y(y) {};
+    Point2d() : m_x(0), m_y(0){};
+    Point2d(float x, float y) : m_x(x), m_y(y){};
 };
 
-class Point3d{
+class Point3d
+{
 public:
     float m_x, m_y, m_z;
-    Point3d() : m_x(0), m_y(0), m_z(0) {};
-    Point3d(float x, float y, float z) : m_x(x), m_y(y), m_z(z) {};
+    Point3d() : m_x(0), m_y(0), m_z(0){};
+    Point3d(float x, float y, float z) : m_x(x), m_y(y), m_z(z){};
 };
 
-class Circle{
+class Circle
+{
 private:
     float m_ratio;
 
 public:
     //Constructors
-    Circle() : m_ratio(0) {};
-    Circle(float x) : m_ratio(x) {};
+    Circle() : m_ratio(0){};
+    Circle(float x) : m_ratio(x){};
     //Calcular a area
-    float CalcArea() const { return (M_PI*m_ratio*m_ratio); };
+    float CalcArea() const { return (M_PI * m_ratio * m_ratio); };
 };
 
-class Square{
+class Square
+{
 private:
     float m_edge;
 
 public:
     //Constructors
-    Square() : m_edge(0) {};
-    Square(float x) : m_edge(x) {};
+    Square() : m_edge(0){};
+    Square(float x) : m_edge(x){};
     //Calcular a area
-    float CalcArea() const { return (m_edge*m_edge); };
-
+    float CalcArea() const { return (m_edge * m_edge); };
 };
 
-class Triangle{
+class Triangle
+{
 private:
-    float m_e1, m_e2,m_e3;
+    float m_e1, m_e2, m_e3;
 
 public:
     //Constructors
-    Triangle() : m_e1(0), m_e2(0), m_e3(0) {};
-    Triangle(Point2d p1, Point2d p2, Point2d p3) {};
-    Triangle(float e1, float e2, float e3) : m_e1(e1), m_e2(e2), m_e3(e3) {};
+    Triangle() : m_e1(0), m_e2(0), m_e3(0){};
+    Triangle(Point2d p1, Point2d p2, Point2d p3);
+    Triangle(float e1, float e2, float e3) : m_e1(e1), m_e2(e2), m_e3(e3){};
     //Calcular a area
-    float CalcArea(); 
-
+    float CalcArea();
 };
 
-class Sphere{
+class Sphere
+{
 private:
     float m_ratio;
 
 public:
     //Constructors
-    Sphere() : m_ratio(0) {};
-    Sphere(float x) : m_ratio(x) {};
+    Sphere() : m_ratio(0){};
+    Sphere(float x) : m_ratio(x){};
     //Calcular a area
-    float CalcArea() const { return (4*M_PI*m_ratio*m_ratio); };
-
+    float CalcArea() const { return (4 * M_PI * m_ratio * m_ratio); };
 };
 
-class Tetrahedron{
+class Tetrahedron
+{
 private:
     Point3d m_p1, m_p2, m_p3, m_p4;
 
 public:
     //Constructors
-    Tetrahedron(){ m_p1=m_p2=m_p3=m_p4=Point3d(0,0,0); };
-    Tetrahedron(Point3d p1, Point3d p2, Point3d p3, Point3d p4) : m_p1(p1), m_p2(p2), m_p3(p3), m_p4(p4) {};
+    Tetrahedron() { m_p1 = m_p2 = m_p3 = m_p4 = Point3d(0, 0, 0); };
+    Tetrahedron(Point3d p1, Point3d p2, Point3d p3, Point3d p4) : m_p1(p1), m_p2(p2), m_p3(p3), m_p4(p4){};
     //Calcular a area
     float CalcArea();
-
 };
 
-class Cube{
+class Cube
+{
 private:
     float m_edge;
 
 public:
     //Constructors
-    Cube() : m_edge(0) {};
-    Cube(float x) : m_edge(x) {};
+    Cube() : m_edge(0){};
+    Cube(float x) : m_edge(x){};
     //Calcular a area
-    float CalcArea() const { return (6*m_edge*m_edge); };
-
+    float CalcArea() const { return (6 * m_edge * m_edge); };
 };
 
 #endif
